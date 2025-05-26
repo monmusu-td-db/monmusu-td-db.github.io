0222f31c912ee77e0de86a7569f6a1f6818ed1cf
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2025-05-26 18:29:11 +0900

    ツールチップ修正

--- components/UI/TooltipControl.tsx
+++ components/UI/TooltipControl.tsx
@@ -19,12 +19,13 @@ import { StatTooltip } from "../Stat/StatTooltip";
 import { Contexts } from "../States";
 
 type TooltipCond = {
+  enabled: boolean;
   show: boolean;
   cell: CellData | null;
 };
 
-const TOOLTIP_DELAY = 35;
-const defaultCond: TooltipCond = { show: false, cell: null };
+const TOOLTIP_DELAY = 5;
+const defaultCond: TooltipCond = { show: false, enabled: false, cell: null };
 
 const TooltipControl = memo(function TooltipControl({
   children,
@@ -46,41 +47,64 @@ const TooltipControl = memo(function TooltipControl({
     ids.clear();
   }
 
+  const isEnabledStat = useCallback(
+    (stat: unknown): stat is StatTooltip<unknown, unknown> => {
+      return stat instanceof StatTooltip && stat.isEnabled(setting);
+    },
+    [setting]
+  );
+
   const handleClick = useCallback((target: CellData) => {
     clearTimers();
     setCond((p) => {
       return {
+        enabled: true,
         show: p.cell?.ref === target.ref ? !p.show : true,
         cell: target,
       };
     });
   }, []);
 
-  const handleMouseOver = useCallback((target: CellData) => {
-    clearTimers();
-    setCond((p) => ({
-      show: false,
-      cell: p.cell,
-    }));
-    const timerId = window.setTimeout(() => {
-      // ちらつきを抑えるために遅延させる
-      timerIds.current.delete(timerId);
-      setCond({
-        show: true,
-        cell: target,
+  const handleMouseOver = useCallback(
+    (target: CellData) => {
+      clearTimers();
+      setCond((p) => {
+        if (!isEnabledStat(target.stat)) {
+          return { enabled: true, show: false, cell: p.cell };
+        } else if (p.cell?.ref === target.ref && p.show && p.enabled) {
+          return p;
+        } else {
+          const timerId = window.setTimeout(() => {
+            // ちらつきを抑えるために遅延させる
+            timerIds.current.delete(timerId);
+            setCond({
+              enabled: true,
+              show: true,
+              cell: target,
+            });
+          }, TOOLTIP_DELAY);
+          timerIds.current.add(timerId);
+
+          return {
+            enabled: false,
+            show: p.show,
+            cell: p.cell,
+          };
+        }
       });
-    }, TOOLTIP_DELAY);
-    timerIds.current.add(timerId);
-  }, []);
+    },
+    [isEnabledStat]
+  );
 
   const handleMouseOut = useCallback((target: CellData) => {
     const timerId = window.setTimeout(() => {
-      timerIds.current.delete(timerId);
+      clearTimers();
       setCond((p) => {
         if (p.cell?.ref !== target.ref) {
           return p;
         } else {
           return {
+            enabled: true,
             show: false,
             cell: p.cell,
           };
@@ -106,21 +130,12 @@ const TooltipControl = memo(function TooltipControl({
 
   const cellRef = cond.cell?.ref ?? null;
   const cellStat = cond.cell?.stat;
-  const placement = "auto";
-  const flip = placement && placement.indexOf("auto") !== -1;
-  const isEnabled =
-    cellStat instanceof StatTooltip && cellStat.isEnabled(setting);
 
   return (
     <CellEventHandlersContext.Provider value={handlers}>
       {children}
-      {isEnabled && (
-        <Overlay
-          target={cellRef}
-          show={cond.show}
-          placement={placement}
-          flip={flip}
-        >
+      {cond.enabled && isEnabledStat(cellStat) && (
+        <Overlay target={cellRef} show={cond.show} placement={"auto"} flip>
           <Popover id="stat-tooltip" placement="auto">
             <Tooltip stat={cellStat} setting={setting} />
           </Popover>
