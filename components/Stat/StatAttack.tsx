0253563a45588deff1aba30aee3cf2632e46a592
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2025-05-28 12:12:23 +0900

    攻撃力実装修正

--- components/Stat/StatAttack.tsx
+++ components/Stat/StatAttack.tsx
@@ -3,8 +3,9 @@ import type { ReactNode } from "react";
 import * as Data from "../Data";
 import { Setting } from "../States";
 import { SituationBaseStat } from "./SituationBaseStat";
-import { Level, Positive, Result } from "../UI/Util";
+import { Tooltip as T } from "../UI/Tooltip";
 
+const sign = T.sign;
 export type Factors = Data.ActualAttackFactors | undefined;
 
 export class StatAttack extends SituationBaseStat<Factors> {
@@ -21,27 +22,12 @@ export class StatAttack extends SituationBaseStat<Factors> {
     const f = this.getFactors(setting);
     if (f === undefined) return;
     const damageFactor = f.damageFactor ?? 100;
-    const damageFactorDesc = (
-      <>
-        {damageFactor !== 100 && (
-          <>
-            {this.multiply}
-            <Level level={damageFactor > 100}>ダメージ倍率</Level>
-          </>
-        )}
-      </>
-    );
-    const damageFactorValue = (
-      <>
-        {damageFactor !== 100 && (
-          <>
-            {this.multiply}
-            <Level level={damageFactor > 100}>
-              {damageFactor + this.percent}
-            </Level>
-          </>
-        )}
-      </>
+    const getDamageFactor = (d: boolean) => (
+      <T.Multiply enabled={damageFactor !== 100}>
+        <T.Value isPositive={damageFactor > 100}>
+          {d ? "ダメージ倍率" : damageFactor + sign.PERCENT}
+        </T.Value>
+      </T.Multiply>
     );
 
     return (
@@ -50,57 +36,49 @@ export class StatAttack extends SituationBaseStat<Factors> {
           <>
             {super.getTooltipBody(setting)}
             {f.criticalChance < 100 && (
-              <>
-                <dt>
-                  <Result>実攻撃力</Result>
-                  {this.equal}
-                  <small>
-                    表示能力値
-                    {damageFactorDesc}
-                  </small>
-                </dt>
-                <dd>
-                  <Result b>{f.actualResult}</Result>
-                  {this.equal}
-                  {f.inBattleResult}
-                  {damageFactorValue}
-                </dd>
-              </>
+              <T.Equation>
+                {(d) => (
+                  <>
+                    <T.Result>{d ? "実攻撃力" : f.actualResult}</T.Result>
+                    <T.Expression>
+                      {d ? "表示能力値" : f.inBattleResult}
+                      {getDamageFactor(d)}
+                    </T.Expression>
+                  </>
+                )}
+              </T.Equation>
             )}
-
             {f.criticalChance > 0 && (
-              <>
-                <dt>
-                  <Result>
-                    <small>
-                      {this.bStart}CRI率{this.bEnd}{" "}
-                    </small>
-                    CRI攻撃力
-                  </Result>
-                  {this.equal}
-                  <small>
-                    表示能力値
-                    {this.multiply}
-                    <Positive>CRIダメージ倍率</Positive>
-                    {damageFactorDesc}
-                  </small>
-                </dt>
-                <dd>
-                  <Result b>
-                    {this.bStart}
-                    {f.criticalChance}
-                    {this.percent}
-                    {this.bEnd}
-                    <> </>
-                    {f.criticalAttack}
-                  </Result>
-                  {this.equal}
-                  {f.inBattleResult}
-                  {this.multiply}
-                  <Positive>{f.criticalDamage + this.percent}</Positive>
-                  {damageFactorValue}
-                </dd>
-              </>
+              <T.Equation>
+                {(d) => {
+                  const criChance = (
+                    <>
+                      {sign.BSTART}
+                      {d ? "CRI率" : f.criticalChance + sign.PERCENT}
+                      {sign.BEND}
+                    </>
+                  );
+                  return (
+                    <>
+                      <T.Result>
+                        {d ? <small>{criChance}</small> : criChance}{" "}
+                        {d ? "CRI攻撃力" : f.criticalAttack}
+                      </T.Result>
+                      <T.Expression>
+                        {d ? "表示能力値" : f.inBattleResult}
+                        <T.Multiply>
+                          <T.Positive>
+                            {d
+                              ? "CRIダメージ倍率"
+                              : f.criticalDamage + sign.PERCENT}
+                          </T.Positive>
+                        </T.Multiply>
+                        {getDamageFactor(d)}
+                      </T.Expression>
+                    </>
+                  );
+                }}
+              </T.Equation>
             )}
           </>
         ) : (
