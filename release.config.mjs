364083ab6146036c3abcb2536bbfa97ac2d4e973
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2025-07-20 12:57:42 +0900

    build: 設定変更

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('semantic-release').GlobalConfig} */
 const releaseConfig = {
-  plugins: ["@semantic-release/commit-analyzer"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+  ],
 };
 
 export default releaseConfig;
