2980b583c7055399ff96058f40bf6f536a685777
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2025-07-21 12:36:33 +0900

    feat: チェンジログ表示修正

--- release.config.mjs
+++ release.config.mjs
@@ -4,23 +4,7 @@
 const releaseConfig = {
   plugins: [
     "@semantic-release/commit-analyzer",
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        presetConfig: {
-          types: [
-            { type: "feat", section: "機能追加" },
-            { type: "fix", section: "バグ修正" },
-            { type: "chore", hidden: true },
-            { type: "docs", hidden: true },
-            { type: "style", hidden: true },
-            { type: "refactor", hidden: true },
-            { type: "perf", hidden: true },
-            { type: "test", hidden: true },
-          ],
-        },
-      },
-    ],
+    "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
     [
