0ea28d8b19f64731f3537f7261045746ca078e74
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2025-08-28 11:33:56 +0900

    fix: 問い合わせページ微修正

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,16 @@
 import Header from "@/components/UI/Header";
+import { Container } from "react-bootstrap";
 
-export default function App() {
-  return <Header />;
+function App() {
+  return (
+    <>
+      <Header />
+      <Container className="md-content">
+        <h1>お問い合わせ</h1>
+        <p>誤字脱字、データの誤認などがあればお知らせください。</p>
+      </Container>
+    </>
+  );
 }
+
+export default App;
