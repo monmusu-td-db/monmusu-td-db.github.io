3797a1d4b0be1d7cb8339bd5555eff3f0ba32f1d
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2025-08-30 18:59:12 +0900

    feat: アイコン切り替え追加

---
+++ components/UI/IconToggler.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import cn from "classnames";
+import Images from "./Images";
+import { useState } from "react";
+
+interface Condition {
+  showDiff: boolean;
+  cooldown: boolean;
+}
+
+const defaultCondition: Condition = {
+  showDiff: false,
+  cooldown: false,
+};
+
+function IconToggler({ className }: { className?: string | undefined }) {
+  const [cond, setCond] = useState(defaultCondition);
+
+  function handleToggle() {
+    if (!cond.cooldown) {
+      setCond({ showDiff: true, cooldown: true });
+      window.setTimeout(() => {
+        setCond((p) => ({ ...p, showDiff: false }));
+      }, 4000);
+      window.setTimeout(() => {
+        setCond(defaultCondition);
+      }, 4500);
+    }
+  }
+
+  return (
+    <>
+      <Images.Icon1
+        className={cn(className, { "d-none": cond.showDiff })}
+        onClick={handleToggle}
+        onMouseOver={handleToggle}
+      />
+      <Images.Icon2 className={cn(className, { "d-none": !cond.showDiff })} />
+    </>
+  );
+}
+
+export default IconToggler;
