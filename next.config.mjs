149687cb13e5a8e017d7c99f3141004665b7a11e
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2025-11-20 18:39:14 +0900

    build: パッケージアップデート

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   devIndicators: false,
   output: "export",
   images: { unoptimized: true },
+  sassOptions: {
+    silenceDeprecations: [
+      "mixed-decls",
+      "color-functions",
+      "global-builtin",
+      "import",
+    ],
+  },
 };
 
 export default nextConfig;
