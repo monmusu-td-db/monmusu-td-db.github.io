0e319f9602e9f4734dcf7f8511cf48c302a40b68
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2026-07-28 17:24:20 +0900

    feat: ユニット追加446

--- components/Data.ts
+++ components/Data.ts
@@ -1829,6 +1829,7 @@ const potentialList = [
   { name: "スキル時自マス変化：地", stat: null, value: 1 },
   { name: "呪詛の敵へ与ダメ1.3倍", stat: null, value: 1 },
   { name: "毒の敵へ与ダメ1.2倍", stat: null, value: 1 },
+  { name: "スキル中与ダメ1.2倍", stat: null, value: 1 },
 ] as const satisfies PotentialEffect[];
 export type Potential = (typeof potentialList)[number]["name"];
 export const Potential = {
