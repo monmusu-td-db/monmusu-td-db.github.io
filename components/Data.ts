5e5a78aade906b73b2d7125ce3e63c60c1311e39
Kaidon102 <199693979+Kaidon102@users.noreply.github.com>
2026-08-26 19:54:25 +0900

    fix: Invalid Potentials

--- components/Data.ts
+++ components/Data.ts
@@ -1814,6 +1814,7 @@ const potentialList = [
   { name: "HP+600、攻撃力+75", stat: stat.attack, value: 75 },
   { name: "攻撃時スタン蓄積+10", stat: null, value: 10 },
   { name: "攻撃時毒確率+20%", stat: null, value: 1 },
+  { name: "水属性マスボーナス3倍", stat: null, value: 3 },
   { name: "地属性マスボーナス3倍", stat: null, value: 3 },
   { name: "光属性マスボーナス3倍", stat: null, value: 3 },
   { name: "闇属性マスボーナス3倍", stat: null, value: 3 },
